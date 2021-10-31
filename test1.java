82e0f8b8545e4273715556c17f07e213d25cc545
Cudev97 <h.lloyd526@gmail.com>
2021-10-31 14:07:50 -0400

    more test

--- test1.java
+++ test1.java
@@ -2,19 +2,8 @@ package githubTest;
 
 public class test1 {
       // hello
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 	//hello2
-	
-	
-	
-	
-
 	//help
-
-=======
->>>>>>> parent of c5b1d8e (Update test1.java)
-=======
->>>>>>> parent of 67bf0ba (Update test1.java)
 }
