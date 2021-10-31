6c147957dc2eb498a768ce9ded430085961fc30a
Cudev97 <h.lloyd526@gmail.com>
2021-10-31 14:04:12 -0400

    Update test1.java

--- test1.java
+++ test1.java
@@ -2,6 +2,7 @@ package githubTest;
 
 public class test1 {
       // hello
+<<<<<<< HEAD
 
 	//hello2
 	
@@ -11,4 +12,6 @@ public class test1 {
 
 	//help
 
+=======
+>>>>>>> parent of c5b1d8e (Update test1.java)
 }
