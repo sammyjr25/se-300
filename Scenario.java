11c12bebb0ae372c704177fda185c708504d625a
Cudev97 <93484620+Cudev97@users.noreply.github.com>
2021-11-15 13:07:02 -0500

    Sceranrio.java update

--- Scenario.java
+++ Scenario.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileNotFoundException;
@@ -21,59 +22,123 @@ import java.io.FileNotFoundException;
 public class Scenario {
 
 	private File datafile;
-	private ArrayList<String[]> data;
-	
+	private ArrayList<String[]> rawdata;
+	private ArrayList<String[]> sortedData;
 	
 	public Scenario() {
 		//constructor
+		
+		//Database name
 		datafile = new File("Data.csv");
-		data = new ArrayList<String[]>();
-		//file = new File("data.txt");
 		
+		//creates array-lists for later
+		rawdata = new ArrayList<String[]>();
+		sortedData = new ArrayList<String[]>();
 	}
-	public void parseFile() {
-	
+	public ArrayList<String[]> parseDataBase() {
 		
-		//shows where it's looking for the datafile
-		System.out.println(datafile.getAbsolutePath());
 		
-			// reader that works
+		//shows where it's looking for the Data.csv. If error please place Database (.csv) in location below
+		//System.out.println(datafile.getAbsolutePath());
+		
+			// File reader that pulls all information from the database
 			try {
 				Scanner scan = new Scanner(datafile);
 				scan.nextLine();
 				//ArrayList<String[]> input = new ArrayList<String[]>();
 				while(scan.hasNextLine()) {
 					String[] Data = scan.nextLine().split(",");
-					data.add(Data);
+					rawdata.add(Data);
 					}
-				for(int i =0; i< data.size(); i++) {
-					String[] dataj = data.get(i);
-					for(int j = 0; j < dataj.length; j++) {
-						System.out.print(dataj[j] + " ");
-					}
-					System.out.println();
-				}
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 			}
+			//outputs the rawData array-list
+			for(int i =0; i< rawdata.size(); i++) {
+				String[] dataj = rawdata.get(i);
+				for(int j = 0; j < dataj.length; j++) {
+//					System.out.print(dataj[j] + " ");
+				}
+				
+//				System.out.println();
+				// line above creates neat ordering when using the print line in for-loop
+			}
+			
+			return rawdata;
 	}
 
 	
+	public ArrayList<String[]> passengerArrivale() {
+		// Calls the RandomNonRepeating number generator. (size, min, max) 
+		ArrayList<Integer> list = getRandomNonRepeatingIntegers(4, 0, 3);
+	    
+		// This loop reorders the passengers to the order they arrive and adds their order number at the end of each string array in the array list
+		for (int k = 0; k < list.size(); k++) {
+	        
+			// this adds the order number to the end of the string array
+			String[] dataPre = rawdata.get(list.get(k)); 
+			String[] dataPost = new String[dataPre.length + 1];
+			System.arraycopy(dataPre, 0, dataPost, 0, dataPre.length);
+			dataPost[dataPre.length] = Integer.toString(k);
+			
+//			System.out.println("" + Arrays.toString(datak));
+			
+			// creates the sorted array-list
+			sortedData.add(dataPost);
+	    }
+			//outputs the sortedData array-list
+			for(int i =0; i< sortedData.size(); i++) {
+				String[] datas = sortedData.get(i);
+				for(int j = 0; j < datas.length; j++) {
+					System.out.print(datas[j] + " " );
+				}
+				
+				System.out.println();
+				// line above creates neat ordering when using the print line in for-loop
+			}
+	    
+		return sortedData;
+		
+	}
 	
-	
-	
+	// number generator for the non repeating generator
+	public static int getRandomInt(int min, int max) {
+	    Random random = new Random();
+
+	    return random.nextInt((max - min) + 1) + min;
+	}
+
+	public static ArrayList<Integer> getRandomNonRepeatingIntegers(int size, int min,int max) {
+	   
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+	    
+	    // ensures no double numbers
+	    while (numbers.size() < size) {
+	        int random = getRandomInt(min, max);
+
+	        if (!numbers.contains(random)) {
+	            numbers.add(random);
+	        }
+	    }
+
+	    return numbers;
+	}
 	public static void main(String[] args) {
 		Scenario Data = new Scenario();
 		//calls parseFile
-		Data.parseFile();
+		Data.parseDataBase();
+		Data.passengerArrivale();
 		
 		
 		
 		
 	}
 	
-    
+//	String[] dataj = rawdata.get(1); 
+//	System.out.print(dataj[j] + " ");  // singles out sections of array
+	
+	
 	//https://www.codegrepper.com/code-examples/java/java+read+csv+file+into+arraylist
 	// good csv to array
 	
@@ -116,3 +181,4 @@ public class Scenario {
 	
 	
 }
+
